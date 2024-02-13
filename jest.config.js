cae3b5941962a3b65a05fe8eb79c0bb3234d92e9
Morgan Ney <morganney@gmail.com>
2024-02-13 10:09:32 -0600

    feat: module, dual builds.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   collectCoverage: true,
   collectCoverageFrom: ['**/src/**/*.js', '!**/node_modules/**'],
   coverageReporters: ['json', 'lcov', 'text', 'clover', 'text-summary'],
